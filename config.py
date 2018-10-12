bd5f68b603be853b1c79a049bb4466d0f5550b2c
KielRodriguez <kr.rdz.20@gmail.com>
2018-10-12 06:58:27 -0500

    se eliminga configuracion mysql

--- config.py
+++ config.py
@@ -4,13 +4,6 @@ class BaseConfig(object):
    DEBUG = False
    Testing = False
 
-   SECRET_KEY= os.getenv('SECRET_KEY')
-   MYSQL_HOST = os.getenv('MYSQL_HOST')
-   MYSQL_USER = os.getenv('MYSQL_USER')
-   MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-   MYSQL_DB = os.getenv('MYSQL_DB')
-   MYSQL_PORT = int( os.getenv('MYSQL_PORT') )
-
 
 class DevelopmentConfig(BaseConfig):
     DEBUG= True
